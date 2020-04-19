c5f965947969b79c158c32b3402deed6826a5371
Suprojit Nandy <suprojitnandy@Suprojits-MacBook-Pro.local>
2020-04-19 21:49:46 +0800

    Commit with utterance pareser beta version to scout through IEOMCAP dataset

---
+++ utterances_master.py
@@ -0,0 +1,34 @@
+# Improved Script to parse through mutiple files to generate the master file:
+
+import random 
+import os
+import sys
+import csv
+import math
+
+def main():
+
+    # Concatenate all the files here::::
+
+    corpus = []
+    file_list = ['Ses01F_impro01.txt', 'Ses01F_impro02.txt', 'Ses01F_impro03.txt', 'Ses01F_impro04.txt', 'Ses01F_impro05.txt', 'Ses01F_impro06.txt', 'Ses01F_impro07.txt', 'Ses01F_script01_1.txt', 'Ses01F_script01_2.txt', 'Ses01F_script01_2.txt', 'Ses01F_script01_3.txt']
+
+    list_uttances_final = []
+
+    for file_element in file_list:
+
+        with open (file_element, 'r', newline='') as source:
+
+            for line in csv.reader(source):
+
+                corpus.append(line[0])
+
+
+
+    with open('utterances_combined.txt', 'w') as target_file:
+        for line in corpus:
+            target_file.write(line + '\n')
+
+    print('Look for "Utterances_combined.txt" in the same host directory \n ')
+
+main()
